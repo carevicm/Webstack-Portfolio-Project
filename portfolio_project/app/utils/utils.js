ba3eccc24f1cb05aada94605fe42c7537829be63
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-11-08 22:13:52 -0800

    Delete portfolio_project directory

--- portfolio_project/app/utils/utils.js
+++
@@ -1,7 +0,0 @@
-export function getFullYear() {
-  return new Date().getFullYear();
-}
-
-export function getFooterCopy(isIndex) {
-  return isIndex ? "Milan Carevic" : "Milan Carevic - Portfolio Website";
-}
