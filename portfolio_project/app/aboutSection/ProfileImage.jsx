f8c386772d983a173e0441586f3c8199aecdc8e7
carevicm <milancarevic@yahoo.com>
2023-11-10 22:05:07 -0800

    update size about me image

--- portfolio_project/app/aboutSection/ProfileImage.jsx
+++ portfolio_project/app/aboutSection/ProfileImage.jsx
@@ -5,8 +5,8 @@ const ProfileImage = () => (
   <div className="relative mt-20 w-64 h-64 sm:w-72 sm:h-72 md:w-72 md:h-72 lg:w-96 lg:h-96 xl:w-96 xl:h-96 mb- sm:mb-2 md:mb-24 lg:mb-96 object-fill bg-cover rounded-3xl overflow-hidden">
   <Image
       src="/assets/about-image.webp"
-      width={250}
-      height={250}
+      width={500}
+      height={500}
       className="object-cover object-center rounded-3xl"
       priority
       alt="About me image, computer with code on the screen"
