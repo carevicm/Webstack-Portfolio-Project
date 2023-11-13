31af5e26ebd1bb816242223ca9313897d937c0b7
carevicm <milancarevic@yahoo.com>
2023-11-13 13:38:34 -0800

    update useLikeDislike.js

--- portfolio_project/app/likeCounter/useLikeDislike.js
+++ portfolio_project/app/likeCounter/useLikeDislike.js
@@ -13,24 +13,14 @@ const DISLIKE_ACTION = "DISLIKE_ACTION";
 function reducer(state, action) {
   switch (action.type) {
     case LIKE_ACTION:
-      let newDislikeCount =
-        state.activeBtn === "dislike" && state.dislikeCount > 0
-          ? state.dislikeCount - 1
-          : state.dislikeCount;
       return {
         ...state,
         likeCount: state.likeCount + 1,
-        dislikeCount: newDislikeCount,
         activeBtn: "like",
       };
     case DISLIKE_ACTION:
-      let newLikeCount =
-        state.activeBtn === "like" && state.likeCount > 0
-          ? state.likeCount - 1
-          : state.likeCount;
       return {
         ...state,
-        likeCount: newLikeCount,
         dislikeCount: state.dislikeCount + 1,
         activeBtn: "dislike",
       };
